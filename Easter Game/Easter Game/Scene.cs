2ba3c2ce6b2970398df14d7beb2733610dae27a6
S00171013 <s00171013@mail.itsligo.ie>
2018-03-31 18:24:52 +0100

    Default Assets added, currently setting up Scene class.

---
+++ Easter Game/Easter Game/Scene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Easter_Game
+{
+    class Scene
+    {
+        public Scene(Texture2D background, List<T> ItemsToRender)
+        {
+
+        }
+    }
+}
